78a933b659b5c42e913a683649bf25ffd65d9b5e
Dany Yanev <yanev89@gmail.com>
2019-02-16 12:49:55 +0200

    Readme and quickfixes

---
+++ README.md
@@ -0,0 +1,18 @@
+# ZombieWaveFPS
+
+Graduation Thesis at ELSYS
+
+
+
+## Instalation
+
+All assets are handled by <b> Git LFS </b>
+
+
+
+## [Documentation](Documentation/Documentation.docx)
+
+The whole documentation is in <b>Bulgarian</b>
+
+
+<i> The fun begins at chapter 3 </i>
--- Source/ZombieSurvivalFPS/ZombieSurvivalFPSGameMode.cpp
+++ Source/ZombieSurvivalFPS/ZombieSurvivalFPSGameMode.cpp
@@ -15,8 +15,8 @@ AZombieSurvivalFPSGameMode::AZombieSurvivalFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
 	HUDClass = AZombieSurvivalFPSHUD::StaticClass();
